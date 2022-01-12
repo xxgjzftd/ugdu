6f738642a520ad41c6d94d327121f72565826ad9
xx <xxgjzftd@gmail.com>
2022-01-12 16:10:29 +0800

    ~

--- packages/processor/src/compose.ts
+++ packages/processor/src/compose.ts
@@ -7,17 +7,12 @@ type SatisfyHooks<T> = T extends BaseHooks<T> ? T : never
 type ParentHooks<T extends TaskOptions> = UnionToIntersection<T extends TaskOptions<infer U> ? U : never>
 type ParentTaskOptions<T extends TaskOptions[]> = TaskOptions<SatisfyHooks<ParentHooks<T[number]>>>
 
-export const rerun = function <T extends TaskOptions>(child: T) {
-  const parent = new TaskOptions(
-    function () {
-      const { manager } = this
-      manager.task(child).run(true)
-    }
-  )
-  child.addParent(parent)
-  return parent
-}
-
+/**
+ * Compose the children task options in series mode.
+ *
+ * @param children - The task options to be wrapped
+ * @returns The composed task options
+ */
 export const series = function <T extends TaskOptions[]>(...children: T) {
   const parent: ParentTaskOptions<T> = new TaskOptions(
     async function () {
@@ -31,6 +26,12 @@ export const series = function <T extends TaskOptions[]>(...children: T) {
   return parent
 }
 
+/**
+ * Compose the children task options in parallel mode.
+ *
+ * @param children - The task options to be wrapped
+ * @returns The composed task options
+ */
 export const parallel = function <T extends TaskOptions[]>(...children: T) {
   const parent: ParentTaskOptions<T> = new TaskOptions(
     async function () {
