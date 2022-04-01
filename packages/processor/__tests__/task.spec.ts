f9eb72f2493f32ddec52a479efb11049e0750ecb
xx <xxgjzftd@gmail.com>
2022-04-01 09:19:31 +0800

    test(processor): init test

---
+++ packages/processor/__tests__/task.spec.ts
@@ -0,0 +1,58 @@
+import { TaskOptions } from '../src/task'
+import { Processor } from '../src/processor'
+
+describe('The TaskOptions class', () => {
+  describe('The addChild method', () => {
+    it('should should work correctly', () => {
+      const parent = new TaskOptions(function () {})
+      const child = new TaskOptions(function () {})
+      expect(parent.children).toEqual([])
+      parent.addChild(child)
+      expect(parent.children).toEqual([child])
+    })
+
+    it('should return the task options instance', () => {
+      const parent = new TaskOptions(function () {})
+      const child = new TaskOptions(function () {})
+      expect(parent.addChild(child)).toBe(parent)
+    })
+  })
+
+  describe('The setHooks method', () => {
+    it('should should work correctly', () => {
+      const to = new TaskOptions<{ 'hook'(): void }>(
+        function () {
+          this.call('hook', 'parallel')
+        },
+        ['hook']
+      )
+      expect(to.hooks).toEqual({})
+      const hooks = { hook: jest.fn() }
+      to.setHooks(hooks)
+      expect(to.hooks).toBe(hooks)
+    })
+
+    it('should return the task options instance', () => {
+      const to = new TaskOptions(function () {})
+      const hooks = { hook: jest.fn() }
+      expect(to.setHooks(hooks)).toBe(to)
+    })
+  })
+})
+
+describe('The Task class', () => {
+  it('should respect the force args', () => {
+    const to = new TaskOptions(function () {})
+    const processor = new Processor()
+    const task = processor.task(to)
+    const action = jest.spyOn(task, 'action')
+    task.run()
+    task.run()
+    task.run()
+    expect(action).toHaveBeenCalledTimes(1)
+    task.run(true)
+    task.run(true)
+    task.run(true)
+    expect(action).toHaveBeenCalledTimes(4)
+  })
+})
